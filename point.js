30edf397319e463470f6553e9cfd9e5a9b9cb1b3
maxim1955 <nikitin.maxim.99@gmail.com>
2024-03-17 18:22:15 +0300

    add labyrinth

--- point.js
+++
@@ -1,9 +0,0 @@
-function createPoint() {
-    const pointBlock = document.createElement('div');
-    pointBlock.classList.add('game__point');
-    pointBlock.textContent = '0';
-
-    return pointBlock;
-}
-
-export default createPoint;
