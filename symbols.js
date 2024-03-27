d2b732c9de473f55a104365bb96f541b6ab74846
Tatya <Tatya-Gabrielyan@mail.ru>
2024-03-27 13:38:35 +0300

    добавила связку с бэком

--- symbols.js
+++ symbols.js
@@ -5,7 +5,7 @@ import createRulesTablet from './rules-tablet.js';
 import { createFindExtra } from './find-extra.js';
 
 
-export function createGameSymbols() {
+export function createGameSymbols(info, number) {
     const game = document.createElement('section');
     const gameTitle = document.createElement('h1');
     const gameSubtitle = document.createElement('h2');
@@ -20,84 +20,12 @@ export function createGameSymbols() {
 
     let points = JSON.parse(localStorage.getItem('points'));
     pointBlock.textContent = points;
-    const items = [
-        {
-            id: 1,
-            img: 'img/symbol-1.svg',
-            inputs: [
-                {
-                    correct: false,
-                    name: 'Солнце, лад, созвучие',
-                },
-                {
-                    correct: true,
-                    name: 'Братская помощь, солидарность',
-                },
-                {
-                    correct: false,
-                    name: 'Небесный свод, вселенная',
-                },
-            ]
 
-        },
-        {
-            id: 2,
-            img: 'img/symbol-2.svg',
-            inputs: [
-                {
-                    correct: true,
-                    name: 'Духовная и родовая связь',
-                },
-                {
-                    correct: false,
-                    name: 'Дом, кров, домашний приют'
-                },
-                {
-                    correct: false,
-                    name: 'Эпоха, период, течение времени'
-                },
+    console.log(info)
 
-            ]
-        },
-        {
-            id: 3,
-            img: 'img/symbol-3.svg',
-            inputs: [
-                {
-                    correct: false,
-                    name: 'Верность, единство, любовь',
-                },
-                {
-                    correct: false,
-                    name: 'Жизненный путь, уклад',
-                },
-                {
-                    correct: true,
-                    name: 'Мысль, знание',
-                },
-            ]
-        },
-        {
-            id: 4,
-            img: 'img/symbol-4.svg',
-            inputs: [
-                {
-                    correct: true,
-                    name: 'Сила семьи, дух нации',
-                },
-                {
-                    correct: false,
-                    name: 'Мера грехов и доброты',
-                },
-                {
-                    correct: false,
-                    name: 'Равенство, понимание и принятие',
-                },
-            ]
-        }
-    ]
+    const questions = Object.values(info[number].questions)
 
-    items.forEach(item => {
+    questions.forEach(item => {
         const symbolItem = document.createElement('div');
         const symbolItemBlock = document.createElement('div');
         const symbolImg = document.createElement('img');
@@ -106,90 +34,265 @@ export function createGameSymbols() {
         symbolItemBlock.classList.add('symbol__block');
         symbolImg.classList.add('symbol__img');
 
-        symbolImg.src = item.img;
+        symbolImg.src = item.pic;
 
-        item.inputs.forEach(el => {
-            const label = document.createElement('label');
-            const input = document.createElement('input');
-            const button = document.createElement('span');
+        const values = Object.values(item.var);
 
-            button.textContent = el.name
+        values.forEach(el => {
+            if (el !== '') {
+                const label = document.createElement('label');
+                const input = document.createElement('input');
+                const button = document.createElement('span');
 
-            label.classList.add('symbol__label');
-            input.classList.add('symbol__input');
-            button.classList.add('symbol__radio');
+                button.textContent = el
 
-            if (el.correct == true) input.classList.add('symbol__input--true');
-            if (el.correct == false) input.classList.add('symbol__input--false');
+                label.classList.add('symbol__label');
+                input.classList.add('symbol__input');
+                button.classList.add('symbol__radio');
 
-            input.type = 'radio';
-            input.name = `symbol-${item.id}`;
-            symbolItemBlock.append(label);
-            label.append(input, button);
+                if (el === item.answer) input.classList.add('symbol__input--true');
+                else input.classList.add('symbol__input--false');
 
-            input.addEventListener('change', (e) => {
-                input.classList.add('checked')
-                const attr = input.getAttribute('name');
-                const symbols = document.querySelectorAll(`[name="${attr}"]`);
-                symbols.forEach(el => el.disabled = true)
+                input.type = 'radio';
+                input.name = `symbol-${questions.indexOf(item)}`;
+                symbolItemBlock.append(label);
+                label.append(input, button);
 
-                const inputs = document.querySelectorAll('.symbol__input.checked');
+                input.addEventListener('change', (e) => {
+                    input.classList.add('checked')
+                    const attr = input.getAttribute('name');
+                    const symbols = document.querySelectorAll(`[name="${attr}"]`);
+                    symbols.forEach(el => el.disabled = true)
 
-                if (inputs.length == 4) {
-                    const inputsTrue = document.querySelectorAll('.symbol__input.checked.symbol__input--true');
-                    if (inputsTrue.length < 2) {
-                        const deniska = createDeniska('К сожалению, угаданы не все значения символов!');
-                        deniska.rulesDeniska.src = 'img/deniska-sad.webp';
-                        deniska.gameBtnNext.addEventListener('click', (e) => {
-                            e.preventDefault();
-                            document.body.innerHTML = '';
-                            const findExtra = createFindExtra();
-                            document.body.append(findExtra);
-                        })
+                    const inputs = document.querySelectorAll('.symbol__input.checked');
 
-                        setTimeout(() => {
-                            document.body.append(deniska.deniska);
-                            document.querySelector('.game__btn--skip').style.display = 'none';
-                            document.querySelector('.game__btn--next').style.display = 'block';
-                        }, 800)
+                    if (inputs.length == 4) {
+                        const inputsTrue = document.querySelectorAll('.symbol__input.checked.symbol__input--true');
+                        if (inputsTrue.length < 2) {
+                            const deniska = createDeniska('К сожалению, угаданы не все значения символов!');
+                            deniska.rulesDeniska.src = 'img/deniska-sad.webp';
+                            deniska.gameBtnNext.addEventListener('click', (e) => {
+                                e.preventDefault();
+                                document.body.innerHTML = '';
+                                const findExtra = createFindExtra();
+                                document.body.append(findExtra);
+                            })
+
+                            setTimeout(() => {
+                                document.body.append(deniska.deniska);
+                                document.querySelector('.game__btn--skip').style.display = 'none';
+                                document.querySelector('.game__btn--next').style.display = 'block';
+                            }, 800)
+
+                        }
+                        else {
+                            let points = JSON.parse(localStorage.getItem('points'));
+                            points += 1;
+                            localStorage.setItem('points', points)
+                            const point = document.querySelector('.game__point');
+                            point.textContent = points;
+                            point.classList.add('animation');
+
+                            const deniska = createDeniska('Отлично! Задание выполнено. Тебе начислен 1 балл.');
+
+                            deniska.gameBtnNext.addEventListener('click', (e) => {
+                                e.preventDefault();
+                                document.body.innerHTML = '';
+                                const findExtra = createFindExtra();
+                                document.body.append(findExtra);
+                            })
+
+                            setTimeout(() => {
+                                document.querySelector('.game__btn--skip').style.display = 'none';
+                                document.querySelector('.game__btn--next').style.display = 'block';
+                                document.body.append(deniska.deniska);
+                            }, 800)
+
+
+                        }
 
                     }
-                    else {
-                        let points = JSON.parse(localStorage.getItem('points'));
-                        points += 1;
-                        localStorage.setItem('points', points)
-                        const point = document.querySelector('.game__point');
-                        point.textContent = points;
-                        point.classList.add('animation');
 
-                        const deniska = createDeniska('Отлично! Задание выполнено. Тебе начислен 1 балл.');
-
-                        deniska.gameBtnNext.addEventListener('click', (e) => {
-                            e.preventDefault();
-                            document.body.innerHTML = '';
-                            const findExtra = createFindExtra();
-                            document.body.append(findExtra);
-                        })
-
-                        setTimeout(() => {
-                            document.querySelector('.game__btn--skip').style.display = 'none';
-                            document.querySelector('.game__btn--next').style.display = 'block';
-                            document.body.append(deniska.deniska);
-                        }, 800)
-
-
-                    }
-
-                }
-
-            })
+                })
+            }
         })
-
 
 
         symbolsBlock.append(symbolItem);
         symbolItem.append(symbolImg, symbolItemBlock);
     })
+
+
+    // const items = [
+    //     {
+    //         id: 1,
+    //         img: 'img/symbol-1.svg',
+    //         inputs: [
+    //             {
+    //                 correct: false,
+    //                 name: 'Солнце, лад, созвучие',
+    //             },
+    //             {
+    //                 correct: true,
+    //                 name: 'Братская помощь, солидарность',
+    //             },
+    //             {
+    //                 correct: false,
+    //                 name: 'Небесный свод, вселенная',
+    //             },
+    //         ]
+
+    //     },
+    //     {
+    //         id: 2,
+    //         img: 'img/symbol-2.svg',
+    //         inputs: [
+    //             {
+    //                 correct: true,
+    //                 name: 'Духовная и родовая связь',
+    //             },
+    //             {
+    //                 correct: false,
+    //                 name: 'Дом, кров, домашний приют'
+    //             },
+    //             {
+    //                 correct: false,
+    //                 name: 'Эпоха, период, течение времени'
+    //             },
+
+    //         ]
+    //     },
+    //     {
+    //         id: 3,
+    //         img: 'img/symbol-3.svg',
+    //         inputs: [
+    //             {
+    //                 correct: false,
+    //                 name: 'Верность, единство, любовь',
+    //             },
+    //             {
+    //                 correct: false,
+    //                 name: 'Жизненный путь, уклад',
+    //             },
+    //             {
+    //                 correct: true,
+    //                 name: 'Мысль, знание',
+    //             },
+    //         ]
+    //     },
+    //     {
+    //         id: 4,
+    //         img: 'img/symbol-4.svg',
+    //         inputs: [
+    //             {
+    //                 correct: true,
+    //                 name: 'Сила семьи, дух нации',
+    //             },
+    //             {
+    //                 correct: false,
+    //                 name: 'Мера грехов и доброты',
+    //             },
+    //             {
+    //                 correct: false,
+    //                 name: 'Равенство, понимание и принятие',
+    //             },
+    //         ]
+    //     }
+    // ]
+
+    // items.forEach(item => {
+    //     const symbolItem = document.createElement('div');
+    //     const symbolItemBlock = document.createElement('div');
+    //     const symbolImg = document.createElement('img');
+
+    //     symbolItem.classList.add('symbols__item', 'symbol');
+    //     symbolItemBlock.classList.add('symbol__block');
+    //     symbolImg.classList.add('symbol__img');
+
+    //     symbolImg.src = item.img;
+
+    //     item.inputs.forEach(el => {
+    //         const label = document.createElement('label');
+    //         const input = document.createElement('input');
+    //         const button = document.createElement('span');
+
+    //         button.textContent = el.name
+
+    //         label.classList.add('symbol__label');
+    //         input.classList.add('symbol__input');
+    //         button.classList.add('symbol__radio');
+
+    //         if (el.correct == true) input.classList.add('symbol__input--true');
+    //         if (el.correct == false) input.classList.add('symbol__input--false');
+
+    //         input.type = 'radio';
+    //         input.name = `symbol-${item.id}`;
+    //         symbolItemBlock.append(label);
+    //         label.append(input, button);
+
+    //         input.addEventListener('change', (e) => {
+    //             input.classList.add('checked')
+    //             const attr = input.getAttribute('name');
+    //             const symbols = document.querySelectorAll(`[name="${attr}"]`);
+    //             symbols.forEach(el => el.disabled = true)
+
+    //             const inputs = document.querySelectorAll('.symbol__input.checked');
+
+    //             if (inputs.length == 4) {
+    //                 const inputsTrue = document.querySelectorAll('.symbol__input.checked.symbol__input--true');
+    //                 if (inputsTrue.length < 2) {
+    //                     const deniska = createDeniska('К сожалению, угаданы не все значения символов!');
+    //                     deniska.rulesDeniska.src = 'img/deniska-sad.webp';
+    //                     deniska.gameBtnNext.addEventListener('click', (e) => {
+    //                         e.preventDefault();
+    //                         document.body.innerHTML = '';
+    //                         const findExtra = createFindExtra();
+    //                         document.body.append(findExtra);
+    //                     })
+
+    //                     setTimeout(() => {
+    //                         document.body.append(deniska.deniska);
+    //                         document.querySelector('.game__btn--skip').style.display = 'none';
+    //                         document.querySelector('.game__btn--next').style.display = 'block';
+    //                     }, 800)
+
+    //                 }
+    //                 else {
+    //                     let points = JSON.parse(localStorage.getItem('points'));
+    //                     points += 1;
+    //                     localStorage.setItem('points', points)
+    //                     const point = document.querySelector('.game__point');
+    //                     point.textContent = points;
+    //                     point.classList.add('animation');
+
+    //                     const deniska = createDeniska('Отлично! Задание выполнено. Тебе начислен 1 балл.');
+
+    //                     deniska.gameBtnNext.addEventListener('click', (e) => {
+    //                         e.preventDefault();
+    //                         document.body.innerHTML = '';
+    //                         const findExtra = createFindExtra();
+    //                         document.body.append(findExtra);
+    //                     })
+
+    //                     setTimeout(() => {
+    //                         document.querySelector('.game__btn--skip').style.display = 'none';
+    //                         document.querySelector('.game__btn--next').style.display = 'block';
+    //                         document.body.append(deniska.deniska);
+    //                     }, 800)
+
+
+    //                 }
+
+    //             }
+
+    //         })
+    //     })
+
+
+
+    //     symbolsBlock.append(symbolItem);
+    //     symbolItem.append(symbolImg, symbolItemBlock);
+    // })
 
 
     gameBtnSkip.addEventListener('click', (e) => {
@@ -213,10 +316,13 @@ export function createGameSymbols() {
         deniska.rulesText.append(btns);
 
         yesBtn.addEventListener('click', (e) => {
-            e.preventDefault();
             document.body.innerHTML = '';
-            const findExtra = createFindExtra();
-            document.body.append(findExtra);
+            console.log(info.length, number)
+            if (info.length === 1 || number === info.length - 1) {
+                createPuzzleGame();
+            } else {
+                createGameSymbols(info, number + 1);
+            }
         })
 
         noBtn.addEventListener('click', (e) => {
@@ -233,13 +339,17 @@ export function createGameSymbols() {
     gameBtnNext.addEventListener('click', (e) => {
         e.preventDefault();
         document.body.innerHTML = '';
-        // const puzzle = createPuzzleGame();
-        // document.body.append(puzzle);
-        const findExtra = createFindExtra();
-        document.body.append(findExtra);
+        console.log(info.length, number)
+        if (info.length === 1 || number === info.length - 1) {
+            createPuzzleGame();
+        } else {
+            createGameSymbols(info, number + 1);
+        }
+
     })
 
-    const gameRules = createTalker('Что обозначают эти символы? Выбери правильный вариант ответа.');
+    // const gameRules = createTalker('Что обозначают эти символы? Выбери правильный вариант ответа.');
+    const gameRules = createTalker(info[number].title);
 
     game.classList.add('game', 'symbols');
     gameTitle.classList.add('game__title');
@@ -253,7 +363,8 @@ export function createGameSymbols() {
 
 
     gameTitle.textContent = 'Краеведческий калейдоскоп';
-    gameSubtitle.innerHTML = `Символы <span class="blue-text">Чувашии</span> и их значение`;
+    gameSubtitle.innerHTML = info[number].title
+    // gameSubtitle.innerHTML = `Символы <span class="blue-text">Чувашии</span> и их значение`;
     gameBtnSkip.textContent = 'Пропустить игру';
     gameBtnNext.textContent = 'Следующая игра';
 
