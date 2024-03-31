f5ededdacc5a402061ae1cb52a2ba7f8ff882e4c
Виктория Ознобихина <O.Vikki@gmail.com>
2024-03-31 07:33:35 +0300

    Поправила пазл, кнопка скачать

--- puzzle.js
+++ puzzle.js
@@ -207,13 +207,9 @@ export function createPuzzleGame(info, number) {
 
           // Логика обработки перетаскивания, аналогичная dragEnd
           let currImg = currTile.src;
-          let currIndex = pieces.indexOf(
-            currImg.split("puzzle")[1].split(".")[0]
-          );
+          let currIndex = pieces.indexOf(currImg.split("puzzle/")[1].split('puzzle')[1].split(".")[0]);
           let otherImg = otherTile.src;
-          let otherIndex = pieces.indexOf(
-            otherImg.split("puzzle")[1].split(".")[0]
-          );
+          let otherIndex = pieces.indexOf(otherImg.split("puzzle/")[1].split('puzzle')[1].split(".")[0]);
 
           currTile.src = otherImg;
           otherTile.src = currImg;
